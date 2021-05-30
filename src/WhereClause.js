d6aa8b3eba8948f39d3b06b402f6f224b14bee46
James Bratt <hello@jamesbratt.co.uk>
2021-05-30 10:22:54 +0100

    Some css improvements for the form layout

--- src/WhereClause.js
+++ src/WhereClause.js
@@ -16,19 +16,20 @@ const WhereClause = ({ config, index, json, update, remove }) => {
     }
 
     return (
-        <div className="clause">
-            <div className="clause-header"><span>Where...</span>
-            <JsonAutocomplete data={json} onSelect={updateLeftOperand} />
-            <select className="form-control" value={config.operator} onChange={(e) => updateOperator(e.target.value)}>
-                <option value="EQUAL_TO">Is Equal To</option>
-                <option value="NOT_EQUAL_TO">Is not Equal To</option>
-                <option value="GREATER_THAN">Is greater Than</option>
-                <option value="LESS_THAN">Is Less Than</option>
-                <option value="GREATER_THAN_EQUAL_TO">Is greater Than Or Equal To</option>
-                <option value="LESS_THAN_EQUAL_TO">Is Less Than Or Equal To</option>
-            </select> 
-            <JsonAutocomplete data={json} onSelect={updateRightOperand} />
-            <button onClick={() => remove(index)} className="remove-clause">Remove Clause</button></div>
+        <div className="form-block">
+            <div className="clause">
+                <div className="clause-header"><span>Where...</span><button onClick={() => remove(index)} className="remove-clause">Remove Clause</button></div>
+                <JsonAutocomplete data={json} onSelect={updateLeftOperand} />
+                <select className="form-control" value={config.operator} onChange={(e) => updateOperator(e.target.value)}>
+                    <option value="EQUAL_TO">Is Equal To</option>
+                    <option value="NOT_EQUAL_TO">Is not Equal To</option>
+                    <option value="GREATER_THAN">Is greater Than</option>
+                    <option value="LESS_THAN">Is Less Than</option>
+                    <option value="GREATER_THAN_EQUAL_TO">Is greater Than Or Equal To</option>
+                    <option value="LESS_THAN_EQUAL_TO">Is Less Than Or Equal To</option>
+                </select> 
+                <JsonAutocomplete data={json} onSelect={updateRightOperand} />
+            </div>
         </div>
     )
 }
